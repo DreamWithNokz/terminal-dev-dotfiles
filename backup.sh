f392a7ae52d4337269b800915f911df9c7587836
Nokz <dreamwithnokz@gmail.com>
2021-11-14 02:59:57 +0800

    Add scripts (backup & restore)

---
+++ backup.sh
@@ -0,0 +1,24 @@
+# Files to backup
+dotfiles=(
+  ~/.tmux.conf
+  ~/.zshrc
+  ~/.config/nvim/init.vim
+)
+
+# Creates initial folder then clean it if it exists
+mkdir ./dotfiles -v
+rm -rf ./dotfiles/* -v
+
+# Copy files to dotfiles folder
+cp ${dotfiles[*]} ./dotfiles/ -rv
+cd dotfiles
+
+# Ignore this for files on root folder
+# The commands from here are for multiple directories
+mkdir ./.config -v
+rm -rf ./.config/* -v
+mkdir ./.config/nvim -v
+rm -rf ./.config/nvim/* -v
+
+# Configs with subdirectories inside config
+mv ./init.vim ./.config/nvim -v
